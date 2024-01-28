bb7c78a1ea5e3d9fbaac14e74b6b2b2e716e1ee2
Swastik Mukherjee <82890981+aarush202070@users.noreply.github.com>
2024-01-28 15:12:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+function addUser()
+{
+
+    window.location = "index.html";
+}
+function addUser1()
+{
+
+    window.location = "main.html";
+}
+function addUser2()
+{
+
+    window.location = "terms.html";
+}
+function addUser3()
+{
+
+    window.location = "motion.html";
+}
+
+function addUser4()
+{
+    window.location = "Position.html";
+}
+function addUser5()
+{
+
+    window.location = "GSL.html";
+}
+function addUser6()
+{
+
+    window.location = "cacus.html";
+}
+function addUser7()
+{
+    window.location = "Resolution.html";
+}
+function addUser8()
+{
+
+    window.location = "crisis2.html";
+}
+function addUser9()
+{
+
+    window.location = "research2.html";
+}
+
+function addUser10()
+{
+
+    window.location = "faq.html";
+}
